ca38bebb3703c4cddcf1786ba14e97099143b40c
sdas <sdas2k20@gmail.com>
2023-09-17 11:50:04 +0530

    sending token to jwt

--- keys.js
+++ keys.js
@@ -1,4 +1,5 @@
 module.exports={
-    MONGOURI:"mongodb+srv://sdas2k19:hxQ582PVJ4TE3xGM@cluster0.yscmpwv.mongodb.net/"
+    MONGOURI:"mongodb+srv://sdas2k19:hxQ582PVJ4TE3xGM@cluster0.yscmpwv.mongodb.net/",
+    JWT_SECRET:"qwertyuiopoiuytrewq"
 }
 //hxQ582PVJ4TE3xGM
--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,8 @@ const router = express.Router()
 const mongoose = require('mongoose')
 const User = mongoose.model("User")
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const {JWT_SECRET} = require('../keys')
 
 
 // router.get('/',(req,res)=>{
@@ -58,7 +60,9 @@ router.post('/signin',(req,res)=>{
         bcrypt.compare(password,savedUser.password)
         .then(doMatch=>{
             if(doMatch){
-                res.json({message:"successfully signed in"})
+                //res.json({message:"successfully signed in"})
+                const token = jwt.sign({id:savedUser._id}, JWT_SECRET)
+                res.json({token})
             }
             else{
                 return res.status(422).json({error:"invalid Email or password"})
